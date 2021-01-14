ba3f0defd2eacb809751cb8af7d4aa147152b808
navin dalmia <navindalmia2003@yahoo.co.in>
2021-01-14 15:41:47 +0530

    Changes for v2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import './App.css';import React from 'react';
+import logo from './logo.svg';
 import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from V2</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
 
 class App extends Component {
   render() {
