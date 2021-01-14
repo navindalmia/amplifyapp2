09a34e5a017da78e4b52fd3854ea2ef563fa81ee
navin dalmia <navindalmia2003@yahoo.co.in>
2021-01-14 18:09:44 +0530

    revision

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello Jcy</h1>
+        <h1>Hello </h1>
 
       </header>
        <footer className="App-header">
 
-	          <h1>Developed by Andy</h1>
-	          <h1>DevOps Srabani Upadhyaya</h1>
+	          <h1>Developed by test</h1>
+
       </footer>
     </div>
   );
