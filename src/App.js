8929d46e2a202f7a7a88ed3609ce92d2a125aeb8
navin dalmia <navindalmia2003@yahoo.co.in>
2021-01-14 16:04:05 +0530

    changes to footer

--- src/App.js
+++ src/App.js
@@ -7,8 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>Hello Jcy</h1>
+
       </header>
+       <footer className="App-header">
+
+	          <h1>Developed by Andy</h1>
+	          <h1>DevOps Srabani Upadhyaya</h1>
+      </footer>
     </div>
   );
 }
